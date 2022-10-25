50fd960b9b34bcb6667d14a0f5c7d3488052e425
triccs <105668529+triccs@users.noreply.github.com>
2022-10-25 12:22:53 -0400

    Add files via upload
    Manual package update

---
+++ packages/membrane/src/src/liquidity_check.rs
@@ -0,0 +1,53 @@
+use cosmwasm_std::Addr;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use crate::types::{AssetInfo, LiquidityInfo};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub owner: Option<String>,
+    pub osmosis_proxy: String,
+    pub positions_contract: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    AddAsset {
+        asset: LiquidityInfo,
+    },
+    EditAsset {
+        asset: LiquidityInfo,
+    },
+    RemoveAsset {
+        asset: AssetInfo,
+    },
+    UpdateConfig {
+        owner: Option<String>,
+        osmosis_proxy: Option<String>,
+        positions_contract: Option<String>,
+    },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    Config {},
+    Assets {
+        asset_info: Option<AssetInfo>,
+        limit: Option<u64>,
+        start_after: Option<AssetInfo>,
+    },
+    Liquidity {
+        asset: AssetInfo,
+    },
+}
+
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct Config {
+    pub owner: Addr,
+    pub osmosis_proxy: Addr,
+    pub positions_contract: Addr,
+}
---
+++ packages/membrane/src/src/oracle.rs
@@ -0,0 +1,74 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::{Decimal, Uint128, Addr};
+
+use crate::types::{AssetInfo, AssetOracleInfo, PriceInfo};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub owner: Option<String>,
+    pub osmosis_proxy: String,
+    pub positions_contract: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    UpdateConfig {
+        owner: Option<String>,
+        osmosis_proxy: Option<String>,
+        positions_contract: Option<String>,
+    },
+    AddAsset {
+        asset_info: AssetInfo,
+        oracle_info: AssetOracleInfo,
+    },
+    EditAsset {
+        asset_info: AssetInfo,
+        oracle_info: Option<AssetOracleInfo>,
+        remove: bool,
+    },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    Config {},
+    Price {
+        asset_info: AssetInfo,
+        twap_timeframe: u64,        //in minutes
+        basket_id: Option<Uint128>, //To switch on oracle sources
+    },
+    Prices {
+        asset_infos: Vec<AssetInfo>,
+        twap_timeframe: u64, //in minutes
+    },
+    Asset {
+        asset_info: AssetInfo,
+    },
+    Assets {
+        asset_infos: Vec<AssetInfo>,
+    },
+}
+
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct Config {
+    pub owner: Addr, //MBRN Governance
+    pub osmosis_proxy: Addr,
+    pub positions_contract: Option<Addr>,
+}
+
+// We define a custom struct for each query response
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct PriceResponse {
+    pub prices: Vec<PriceInfo>,
+    pub avg_price: Decimal,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct AssetResponse {
+    pub asset_info: AssetInfo,
+    pub oracle_info: Vec<AssetOracleInfo>,
+}

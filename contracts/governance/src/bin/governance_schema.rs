7f621a00be9925fe2914f3bd64f801ccd2bba8b4
Trix <triccs333@gmail>
2023-08-31 12:30:44 -0400

    Gov: schema

--- contracts/governance/src/bin/governance_schema.rs
+++ contracts/governance/src/bin/governance_schema.rs
@@ -2,7 +2,7 @@ use std::{fs::create_dir_all, env::current_dir};
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use membrane::governance::{InstantiateMsg, ExecuteMsg, QueryMsg, Config, ProposalResponse, ProposalVotesResponse, ProposalListResponse};
+use membrane::governance::{InstantiateMsg, ExecuteMsg, QueryMsg, Config, ProposalResponse, ProposalVotesResponse, ProposalListResponse, Proposal};
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -14,6 +14,7 @@ fn main() {
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(Config), &out_dir);
+    export_schema(&schema_for!(Proposal), &out_dir);
     export_schema(&schema_for!(ProposalResponse), &out_dir);
     export_schema(&schema_for!(ProposalVotesResponse), &out_dir);
     export_schema(&schema_for!(ProposalListResponse), &out_dir); 

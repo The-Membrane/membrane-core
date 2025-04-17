4450841f3e938a9a9f684cc202ee6ebe2564c4d0
triccs <ebmichigan99@gmail.com>
2025-04-16 19:33:40 -0700

    CDP: Fix bad debt liquidations

--- contracts/cdp/src/liquidations.rs
+++ contracts/cdp/src/liquidations.rs
@@ -597,7 +597,10 @@ fn per_asset_fulfillments(
         };
 
         //Subtract Caller fee from Position's claims
-        let caller_fee_in_collateral_amount = pre_user_repay_collateral_repay_amount * caller_fee;
+        let caller_fee_in_collateral_amount = std::cmp::min(
+            pre_user_repay_collateral_repay_amount * caller_fee,
+            collateral_assets[num].asset.amount,
+        );
         
         //Add to caller_fee_value_paid
         let fee_value = match collateral_price.get_value(caller_fee_in_collateral_amount){
@@ -625,8 +628,11 @@ fn per_asset_fulfillments(
         }
         
         //Subtract Protocol fee from Position's claims
-        let protocol_fee_in_collateral_amount = pre_user_repay_collateral_repay_amount * config.clone().liq_fee;
-        
+        let protocol_fee_in_collateral_amount = std::cmp::min(
+            pre_user_repay_collateral_repay_amount * config.clone().liq_fee,
+            collateral_assets[num].asset.amount,
+        );
+                
         //Update collateral_assets to reflect the fee
         collateral_assets[num].asset.amount = match collateral_assets[num].asset.amount.checked_sub(protocol_fee_in_collateral_amount) {
             Ok(res) => res,

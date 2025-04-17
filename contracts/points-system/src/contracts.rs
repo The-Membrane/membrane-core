9e45c7a8dc67658c94b5c08b0b9b837d09ffc726
triccs <ebmichigan99@gmail.com>
2025-04-16 18:38:22 -0700

    Points: change multipliers

--- contracts/points-system/src/contracts.rs
+++ contracts/points-system/src/contracts.rs
@@ -1187,14 +1187,14 @@ pub fn migrate(deps: DepsMut, env: Env, _msg: MigrateMsg) -> Result<Response, Co
 
     //Set initial points multipliers
     let points_multiplier = PointsMultipliers {
-        interest_rate: Decimal::percent(100_00),
+        interest_rate: Decimal::percent(1_00),
         liquidation_execution: Decimal::percent(1_00),
         liquidation_claims: Decimal::percent(1_00),
         governance_votes: Decimal::percent(3_00),
         vault_yields: vec![
             VaultMultiplier {
                 vault_address: String::from(RANGE_BOUND_VAULT),
-                multiplier: Decimal::percent(10_00),
+                multiplier: Decimal::percent(100_00),
             },
         ],
     };

687f302ac72005b88735acfd71c65f036566e7bf
triccs <ebmichigan99@gmail.com>
2025-07-02 20:05:28 -0700

    MM: migrate user

--- contracts/managed-market/src/contract.rs
+++ contracts/managed-market/src/contract.rs
@@ -668,18 +668,12 @@ fn get_user_positions(
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn migrate(deps: DepsMut, env: Env, _msg: MigrateMsg) -> Result<Response, ContractError> {
-    // use membrane::types::UserPosition;
-    // use cosmwasm_std::Decimal;
-    // // Save the provided user position
-    // let user_addr = deps.api.addr_validate("osmo1hfv5gzmpjpgc2ml0qf87j9lrwu9dayq24m33r0")?;
-    // let collateral_denom = "factory/osmo1s794h9rxggytja3a4pmwul53u98k06zy2qtrdvjnfuxruh7s8yjs6cyxgd/umbrn".to_string();
-    // let user_position = UserPosition {
-    //     collateral_denom: collateral_denom.clone(),
-    //     collateral_amount: Uint128::new(409_547_601),
-    //     debt_amount: Uint128::new(66_727),
-    //     rate_index: Decimal::one(),
-    // };
-    // POSITIONS.save(deps.storage, (user_addr, collateral_denom), &user_position)?;
+    // Save the provided user position
+    let user_addr = deps.api.addr_validate("osmo1vf6e300hv2qe7r5rln8deft45ewgyytjnwfrdfcv5rgzrfy0s6cswjqf9r")?;
+    let collateral_denom = "factory/osmo1fqcwupyh6s703rn0lkxfx0ch2lyrw6lz4dedecx0y3ced2jq04tq0mva2l/mars-usdc-tokenized".to_string();
+    let mut user_position = POSITIONS.load(deps.storage, (user_addr.clone(), collateral_denom.clone()))?;
+    user_position.debt_amount = Uint128::new(50_000_000);
+    POSITIONS.save(deps.storage, (user_addr, collateral_denom.clone()), &user_position)?;
     //Return response
     Ok(Response::default())
 }
